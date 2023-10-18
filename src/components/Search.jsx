81f58021e2103375668cdcbb97c78efab5bf03e5
mrdltn <mrdltn@hotmail.com>
2023-10-18 10:14:18 +0700

    add Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+import React from 'react';
+
+function Search({ cb = Function.prototype }) {
+    const [value, setValue] = useState('');
+    const handleKey = (e) => {
+        if (e.key === 'Enter') {
+            handleSubmit();
+        }
+    };
+    const handleSubmit = () => {
+        cb(value);
+    };
+
+    return (
+        <div className='row'>
+            <div className='input-field col s12'>
+                <input
+                    type='search'
+                    name=''
+                    id='search-field'
+                    placeholder='search'
+                    onKeyDown={handleKey}
+                    onChange={(e) => setValue(e.target.value)}
+                    value={value}
+                />
+                <button
+                    className='btn'
+                    style={{ position: 'absolute', top: 0, right: 0 }}
+                    onClick={handleSubmit}
+                >
+                    Search a drink
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export { Search };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,22 +2,34 @@ import { useState, useEffect } from 'react';
 import { getAllDrinksByFirstLetter } from '../api';
 import { Preloader } from '../components/Preloader';
 import { CategoryList } from '../components/CategoryList';
+import { Search } from '../components/Search';
 
 function Home() {
     const [catalog, setCatalog] = useState([]);
+    const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+    const handleSearch = (str) => {
+        setFilteredCatalog(
+            catalog.filter((item) =>
+                item.strDrink.toLowerCase().includes(str.toLowerCase())
+            )
+        );
+    };
 
     useEffect(() => {
         getAllDrinksByFirstLetter().then((data) => {
             setCatalog(data.drinks);
+            setFilteredCatalog(data.drinks);
         });
     }, []);
 
     return (
         <>
+            <Search cb={handleSearch} />
             {!catalog.length ? (
                 <Preloader />
             ) : (
-                <CategoryList catalog={catalog} />
+                <CategoryList catalog={filteredCatalog} />
             )}
         </>
     );
