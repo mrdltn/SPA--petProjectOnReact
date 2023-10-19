9911d4fee99a646705603ce8e9acd235e6e86251
mrdltn <mrdltn@hotmail.com>
2023-10-19 09:41:35 +0700

    use of GET-parameters in Search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { getAllDrinksByFirstLetter } from '../api';
 import { Preloader } from '../components/Preloader';
 import { CategoryList } from '../components/CategoryList';
@@ -8,20 +9,32 @@ function Home() {
     const [catalog, setCatalog] = useState([]);
     const [filteredCatalog, setFilteredCatalog] = useState([]);
 
+    const { search } = useLocation();
+    const navigate = useNavigate();
+
     const handleSearch = (str) => {
         setFilteredCatalog(
             catalog.filter((item) =>
                 item.strDrink.toLowerCase().includes(str.toLowerCase())
             )
         );
+        navigate(`?search=${str}`);
     };
 
     useEffect(() => {
         getAllDrinksByFirstLetter().then((data) => {
             setCatalog(data.drinks);
-            setFilteredCatalog(data.drinks);
+            setFilteredCatalog(
+                search
+                    ? data.drinks.filter((item) =>
+                          item.strDrink
+                              .toLowerCase()
+                              .includes(search.split('=')[1].toLowerCase())
+                      )
+                    : data.drinks
+            );
         });
-    }, []);
+    }, [search]);
 
     return (
         <>
